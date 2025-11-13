ec7ca4a78f664269b07f109c64240cba17e0c8c1
Elias Nimland Lind <elias.nimland.lind1@gmail.com>
2025-11-13 08:52:17 +0100

    Delete installer-scripts/optional directory

--- installer-scripts/optional/software-manager-installer.ps1
+++
@@ -1,9 +0,0 @@
-
-$scriptDir = $PSScriptRoot
-
-Import-Module "$scriptDir\base-installer.psm1"
-
-$sourceDirectory = "$scriptDir\..\scripts\software-manager"
-$destinationDirectory = "C:\Users\Public\scripts\ExecuteOnStartup"
-
-Copy-Scripts -SourceDirectory $sourceDirectory -DestinationDirectory $destinationDirectory
